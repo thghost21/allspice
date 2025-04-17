589d0a53d24f31631b67b84c8499545165cc5f52
thghost21 <tylerhacker21@gmail.com>
2025-04-17 14:15:09 -0600

    env

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-oacrq1f6bky01k4w.us.auth0.com'
+export const clientId = 'Tu17AKrPIUMtsFUJXo9JazoRYFG59MJA'
+export const audience = 'https://auth.hackersandbox.com'
