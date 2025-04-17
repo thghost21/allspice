bb2f23e4119c94a4039052bd1c006e339b941951
thghost21 <tylerhacker21@gmail.com>
2025-04-17 14:44:17 -0600

    Model

--- server/Models/Recipe.cs
+++ server/Models/Recipe.cs
@@ -1,6 +1,12 @@
 namespace allspice.Models;
 
-public class Recipe
+public class Recipe : RepoItem
 {
-  public int MyProperty { get; set; }
+  public string Title { get; set; }
+  public string Instructions { get; set; }
+  public string Img { get; set; }
+  public string Category { get; set; }
+  public string CreatorId { get; set; }
+  public Profile Creator { get; set; }
+
 }
---
+++ server/Models/RepoItem.cs
@@ -0,0 +1,9 @@
+namespace allspice.Models;
+
+public abstract class RepoItem
+{
+  public int Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+
+}
