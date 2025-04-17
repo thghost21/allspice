fc1f068b425b0e98c771ed842dd371e2beb01787
thghost21 <tylerhacker21@gmail.com>
2025-04-17 17:30:49 -0600

    home page and card setup

---
+++ client/src/components/RecipeCard.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div class="recipe-card rounded d-flex flex-column justify-content-between mb-3 "
+    style="background-image: url('https://images.unsplash.com/photo-1652954008429-ce394eacbd7f?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); ">
+    <div class="d-flex justify-content-between">
+      <span class="bg-gray px-2 rounded m-1">Soups</span>
+      <span class="mdi mdi-heart bg-gray m-1 px-2 rounded text-red"></span>
+    </div>
+    <div class="d-flex m-2 bg-gray px-2 rounded ">
+      <span class="fs-4">Fish Bisque</span>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.recipe-card {
+  background-position: center;
+  background-size: cover;
+  min-height: 30dvh;
+}
+
+.card {
+  min-height: 30dvh;
+}
+</style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,13 +1,50 @@
-<script setup >
-import Example from '@/components/Example.vue';
+<script setup>
+import RecipeCard from '@/components/RecipeCard.vue';
+
 
 
 </script>
 
 <template>
-  <Example />
+  <section class="container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-12 bgImg flex-grow">
+        <div class="d-flex flex-fill flex-column align-items-center">
+          <div>
+            <h1 class="text-center">All-Spice</h1>
+            <p class="fs-3 mb-0">Cherish Your Family</p>
+            <p class="fs-3">And Their Cooking</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 text-center">
+        <div>
+          <div class="btn-group shadow " role="group" aria-label="Basic radio toggle button group">
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+            <label class="btn fs-4 btn-outline-success " for="btnradio1">Home</label>
+
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+            <label class="btn fs-4 btn-outline-success" for="btnradio2">My Recipes</label>
+
+            <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+            <label class="btn fs-4 btn-outline-success" for="btnradio3">Favorites</label>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <RecipeCard />
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
-
+.bgImg {
+  background-image: url("https://images.unsplash.com/photo-1483137140003-ae073b395549?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  min-height: 45dvh;
+  background-position: center;
+  background-size: cover;
+}
 </style>
